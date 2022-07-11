da2ea7ce38164944af915334db465d76a9744dbf
Gabriel Noro <gabrielc@comprovei.com>
2022-07-10 23:49:24 -0300

    correcao cors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -9,9 +9,8 @@ import '@shared/typeorm';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
-
 app.use(routes);
 
 app.use(errors());
